5f60b2592a6f90980a44055b86ab39805654aa99
sleep_commit <12345qwse@gmail.com>
2025-04-19 16:58:51 +0300

    added terminal respond

--- api/serializer.py
+++ api/serializer.py
@@ -1,4 +1,10 @@
 from rest_framework import serializers
-
+from .models import Terminal
 
 #class ImgSerializer():
+
+
+class TerminalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Terminal
+        fields = ['id', 'map_id', 'user', 'name', 'coord_x', 'coord_y', 'info']
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ImageView, RouteView
+from .views import ImageView, RouteView, TerminalListView
 
 
 urlpatterns = [
     path('/image', ImageView.as_view()),
-    path('/path', RouteView.as_view())
+    path('/path', RouteView.as_view()),
+    path('/terminals', TerminalListView().as_view())
 ]
