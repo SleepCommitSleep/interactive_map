f50b65cb42656125e5fb2a5a074b5fbd7eb0fe93
sleep_commit <12345qwse@gmail.com>
2025-04-19 17:57:05 +0300

    added terminal respond

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Map
+from .models import Map, Terminal
 # Register your models here.
 
 admin.site.register(Map)
+admin.site.register(Terminal)
--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ import os.path
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.core.files.images import ImageFile
 from django.conf import settings
 from .pathfinder import open_mask, find_path, grid_width, grid_height, grid_size
 from .models import Map, Terminal
 from django.shortcuts import get_object_or_404
+from .serializer import TerminalSerializer
 import os
 # Create your views here.
 
@@ -39,3 +40,19 @@ class RouteView(generics.ListAPIView):
         for point_number in range(path.__len__()):
             response[point_number] = path[point_number]
         return JsonResponse(response)
+
+
+class TerminalListView(generics.ListAPIView):
+    def get(self, request):
+        map_id = request.GET.get("map_id")
+        if map_id == None:
+            return Http404
+        terminal_list = Terminal.objects.filter(map_id=int(map_id))
+        return JsonResponse(TerminalSerializer(terminal_list, many=True).data, safe=False)
+
+
+class AddTerminalView(generics.ListAPIView):
+    def post(self, request):
+        point_x = request.GET.get("x")
+        point_y = request.GET.get("y")
+        return JsonResponse({"status": "error"})
